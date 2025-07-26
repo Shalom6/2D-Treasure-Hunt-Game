9bd90e81df71f6413ad3608fd815f3926af7f92b
Shalom Oyewusi <uos690@usask.ca>
2025-07-25 22:05:50 -0600

    Screen settings for chracters and screen size

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,4 +5,10 @@ import javax.swing.*;
 public class GamePanel extends JPanel {
     // Screen settings
     final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3; //scaling the tile size
+
+    final int tileSize = originalTileSize * scale; // 48x48 tile size
+    // Setting the size of the screen
+    final int maxScreenCol = 16; // 16 columns
+    final int maxScreenRow = 12; // 12 rows
 }
