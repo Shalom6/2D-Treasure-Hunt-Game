1057dd28a9ebbaba969a69c4ea7f5ff85331b7d1
Shalom Oyewusi <uos690@usask.ca>
2025-07-25 22:12:05 -0600

    setting screen size to 768x576 and setting background to black

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
     // Screen settings
@@ -11,4 +12,15 @@ public class GamePanel extends JPanel {
     // Setting the size of the screen
     final int maxScreenCol = 16; // 16 columns
     final int maxScreenRow = 12; // 12 rows
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    public GamePanel() {
+        // setting screen size to 768x576 pixels
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        // sets background to black
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+
+    }
 }
